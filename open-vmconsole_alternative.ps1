0efc05728ddde1e2adca4c8b7889f5919217b266
haguman <haguman@hotmail.com>
2019-12-28 02:06:08 +0100

    Add files via upload

---
+++ open-vmconsole_alternative.ps1
@@ -0,0 +1,9 @@
+﻿function open-vmconsole_alternative
+
+{
+
+param($server)
+
+get-vm $server | %{Start-Process -FilePath "C:\Program Files (x86)\VMware\VMware Remote Console\vmrc.exe" -ArgumentList ($UidUtil.GetParentUid( $_.Uid).replace("/VIServer=","vmrc://") + "?moid=" + $_.id.Replace("VirtualMachine-",""))}
+
+}
